fe95cbf167052d4fc2b46b9f3075715e327f984d
divinedab <divyack2@illinois.edu>
2024-02-24 17:03:45 -0600

    added frames, flipped directions, and boundaries

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let canvasContext
 
 let x = 40
 let y = 10
-let speed = 15
-let jump = 3
+let speed = 10
+let jump = 1
+let currentFrame = 1
+let currentDirection = "f"
+let jumped_back = false
 
 window.onload = function() {
     canvas = document.getElementById('animationCanvas');
@@ -13,6 +16,7 @@ window.onload = function() {
     canvasContext.height = window.innerHeight;
 
     createCanvas();
+    setInterval(updateFrame, 100);
     setInterval(createCanvas, 350);
 
 }
@@ -26,30 +30,50 @@ function make_bg() {
     }
 }
 
+function updateFrame() {
+    currentFrame = (currentFrame % 3) + 1;
+}
+
+function flipDirection() {
+    if (currentDirection == "f") {
+        currentDirection = "b"
+    } else {
+        currentDirection = "f"
+    }
+}
 
 function createCanvas() {
+    if (jumped_back) {
+        flipDirection()
+        jumped_back = false
+    }
 
     if (y == 10) {
         y += jump;
     } else {
         y -= jump;
     }
-    let r = Math.floor(Math.random() * 10);
+    let r = Math.floor(Math.random() * 10) + 1;
     console.log(r);
     if (r <= 1) {
         x -= speed;
+        flipDirection()
+        jumped_back = true
     } else {
         x += speed;
     }
+    
     if (x > 960 || x < 40) {
         speed *= -1
+        flipDirection()
     }
+    x = Math.max(40, Math.min(canvas.width - 40, x));
     make_bg();
 
 
     canvasContext.fillStyle = 'blue';
     const sprite = new Image();
-    sprite.src = 'img/dog1.PNG';
+    sprite.src = `img/dog${currentFrame}${currentDirection}.PNG`;
     sprite.onload = function(){
         canvasContext.drawImage(sprite, x, y)
     }
